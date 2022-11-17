58499cb7f3247446e0f381a2229f90cea307e4e2
hvictorleite <hvictorleite@gmail.com>
2022-11-17 13:15:10 -0300

    Criado componente para Item, separando-o de Lista.

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item'
 import style from './Lista.module.scss';
 
 function Lista() {
@@ -20,10 +21,10 @@ function Lista() {
             <h2>Estudos do dia</h2>
             <ul>
                 {tarefas.map((item, index) => (
-                    <li className={style.item} key={index}>
-                        <h3>{item.tarefa}</h3>
-                        <span>{item.tempo}</span>
-                    </li>
+                    <Item key={index} tarefa={item.tarefa} tempo={item.tempo} />
+                    // OU
+                    // <Item key={index} { ...item } />
+                    // Para pegar todas as informações de item e enviar para Item
                 ))}
             </ul>  
         </aside>
