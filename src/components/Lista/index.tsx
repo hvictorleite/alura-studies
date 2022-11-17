c62b4f32e30348a17004b03d902f6eee7b55c431
hvictorleite <hvictorleite@gmail.com>
2022-11-17 13:55:40 -0300

    Utilizado State para adicionar item à tarefas e renderizar na página.

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item'
 import style from './Lista.module.scss';
 
 function Lista() {
-    const tarefas = [{
+    const [tarefas, setTarefas] = useState([{
         tarefa: 'React',
         tempo: '02:00:00'
     },
@@ -14,11 +14,16 @@ function Lista() {
     {
         tarefa: 'Typescript',
         tempo: '03:00:00'
-    }];
+    }]);
 
     return (
         <aside className={style.listaTarefas}>
-            <h2>Estudos do dia</h2>
+            <h2 onClick={() => {
+                    console.log("h2 clicado", tarefas)
+                    setTarefas([...tarefas, {tarefa: "Estudar estado", tempo: "05:00:00"}])
+                }}>
+                Estudos do dia
+            </h2>
             <ul>
                 {tarefas.map((item, index) => (
                     <Item key={index} tarefa={item.tarefa} tempo={item.tempo} />
