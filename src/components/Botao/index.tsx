10aca38444363157f4057eaf5abaa9954e9b8e07
hvictorleite <hvictorleite@gmail.com>
2022-11-17 11:26:06 -0300

    Estilizando React com CSS inline.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,9 +1,33 @@
 import React from "react";
 
+// Maneiras de estilizar com CSS inline no React
 class Botao extends React.Component {
+    // 1 - Criando o estilo direto na tag 
+    // render() {
+    //     return (
+    //         <button style={{backgroundColor: 'blue'}}>
+    //             Botao
+    //         </button>
+    //     )
+    // }
+    // 2 - Criando variável com estilo e depois chamando-a na tag
+    // render() {
+    //     const backgroundColor = 'red'
+    //     return (
+    //         <button style={{backgroundColor}}>
+    //             Botao
+    //         </button>
+    //     )
+    // }
+    // 3 - Criando variável com estilo, externando para uma outra varivável e depois chamando-a na tag
     render() {
+        const estaAtivo = true;
+        const backgroundColor = estaAtivo ? 'green' : 'yellow'
+        const styles = { backgroundColor }
         return (
-            <button>Botao</button>
+            <button style={ styles }>
+                Botao
+            </button>
         )
     }
 }
