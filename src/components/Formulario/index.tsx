486da25f37d806f560ac223cc5d06ab74e9ad653
hvictorleite <hvictorleite@gmail.com>
2022-12-14 10:16:31 -0300

    Trabalhando com state para adicionar tarefa.

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,22 +1,39 @@
 import React from "react";
 import Botao from '../Botao';
-import style from './Formulario.module.scss'
+import style from './Formulario.module.scss';
 
 class Formulario extends React.Component {
+    state = {
+        tarefa: "",
+        tempo: ""
+    };
+
+adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+    evento.preventDefault();
+    console.log('state: ', this.state)
+}
+
     render() {
         return (
-            <form method="POST" className={style.novaTarefa}>
+            <form method="POST" className={style.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={style.inputContainer}>
                     <label htmlFor="tarefa">Adicione um novo estudo</label>
-                    <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar?" required />
+                    <input type="text" name="tarefa" id="tarefa"
+                        value={this.state.tarefa}
+                        onChange={evento => this.setState({...this.state, tarefa: evento.target.value})}
+                        placeholder="O que você quer estudar?" required />
                 </div>
                 <div className={style.inputContainer}>
                     <label htmlFor="tempo">Tempo</label>
-                    <input type="time" step="1" name="tempo" id="tempo" min="00:00:00" max="01:30:00" />
+                    <input type="time" step="1" name="tempo"
+                        value={this.state.tempo} onChange={evento => this.setState({
+                            ...this.state, tempo: evento.target.value
+                        })} id="tempo"
+                        min="00:00:00" max="01:30:00" />
                 </div>
                 <Botao texto="Adicionar" />
             </form>
-        )
+        );
     }
 }
 
