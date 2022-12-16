44671b16b9026e8d5cfe3fa1c114dae6641694e5
hvictorleite <hvictorleite@gmail.com>
2022-12-16 10:09:36 -0300

    Finalizar tarefa ao findar da regressiva.

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -8,13 +8,15 @@ interface Props extends ITarefa {
 export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props ){
     console.log('item atual: ', { tarefa, tempo, selecionado, completado, id });
     return (
-        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''}`}
+        <li className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado: ''}`}
             onClick={
-                () => selecionaTarefa({ tarefa, tempo, selecionado, completado, id })
+                () => !completado && selecionaTarefa({ tarefa, tempo, selecionado, completado, id })
             }
         >
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
+            {completado &&
+            <span className={style.concluido} aria-label="item"></span>}
         </li>
     )
 }
