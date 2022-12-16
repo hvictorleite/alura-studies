4f04297c57abc4a5aab90b45955398e4171a3c59
hvictorleite <hvictorleite@gmail.com>
2022-12-16 10:33:13 -0300

    Refatoradas class components, convertidas em function components.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,15 +1,16 @@
-import React from "react";
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{ type?: "button" | "submit" | "reset" | undefined,
-                                      texto: string,
-                                      onClick?: () => void }>{
-    render() {
-        const { type = "button", texto, onClick } = this.props;
-        return (
-            <button onClick={onClick} type={type} className={style.botao}>{texto}</button>
-        )
-    }
+interface Props {
+    type?: "button" | "submit" | "reset" | undefined,
+    onClick?: () => void,
+    children: React.ReactNode
+}
+
+
+function Botao({ type, onClick, children }: Props) {
+    return (
+        <button onClick={onClick} type={type} className={style.botao}>{children}</button>
+    );
 }
 
 export default Botao;
