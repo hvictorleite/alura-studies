04ea0aad1163fb6054b3c6be4254cb166096464e
hvictorleite <hvictorleite@gmail.com>
2022-12-16 09:53:17 -0300

    Criada função de contagem regressiva para relógio.

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import style from './Botao.module.scss';
 
-class Botao extends React.Component<{ type?: "button" | "submit" | "reset" | undefined, texto: string }>{
+class Botao extends React.Component<{ type?: "button" | "submit" | "reset" | undefined,
+                                      texto: string,
+                                      onClick?: () => void }>{
     render() {
-        const { type = "button", texto } = this.props;
+        const { type = "button", texto, onClick } = this.props;
         return (
-            <button type={type} className={style.botao}>{texto}</button>
+            <button onClick={onClick} type={type} className={style.botao}>{texto}</button>
         )
     }
 }
